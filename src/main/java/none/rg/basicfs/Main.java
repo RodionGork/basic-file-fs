8b1de067a288238eeef4dcf13b9bdba20981cda1
Rodion Gorkovenko <RodionGork@github.com>
2016-04-23 11:41:06 +0300

    Set up basic things in pom.xml etc.

---
+++ src/main/java/none/rg/basicfs/Main.java
@@ -0,0 +1,13 @@
+package none.rg.basicfs;
+
+/**
+ * For now it is going to be used for manual-testing purposes
+ * Later we shall make it entry point for utility tool
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hi, People!");
+    }
+
+}
--- src/test/java/none/rg/basicfs/BasicFsTest.java
+++ src/test/java/none/rg/basicfs/BasicFsTest.java
@@ -1,4 +1,12 @@
 package none.rg.basicfs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BasicFsTest {
+
+    @Test
+    public void sillyTest() {
+        Assert.assertNotNull(new BasicFs());
+    }
 }
